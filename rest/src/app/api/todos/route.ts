0c64084e7dee7a0672c18da5f41422ebfa1d6588
Vinícius Massari <stonefields11@hotmail.com>
2023-06-01 10:55:33 -0300

    first examples

---
+++ rest/src/app/api/todos/route.ts
@@ -0,0 +1,77 @@
+import { NextResponse } from "next/server"
+
+const API_KEY: string | undefined = process.env.DATA_API_KEY 
+
+
+
+const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+export async function GET(){
+    const todos = await fetch(DATA_SOURCE_URL);
+    const todosJson: Todo[] = await todos.json();
+    return NextResponse.json(todosJson);
+}
+
+export async function DELETE(request: Request){
+    const {id}: Partial<Todo> = await request.json()
+
+    if (!id) {
+        return NextResponse.json({"message": "Todo Id Required"})
+    }
+
+    await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'DELETE',
+        headers:{
+            'Content-Type': 'application/json',
+            'API-KEY': API_KEY ?? ''
+        }
+    })
+
+    return NextResponse.json({"message": `Todo ${id} deleted`})
+}
+
+export async function POST(request: Request){
+    const {userId,title}: Partial<Todo> = await request.json()
+
+    if (!userId || !title) {
+        return NextResponse.json({"message": "Missing required data"})
+    }
+
+  const res =  await fetch(`${DATA_SOURCE_URL}`, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'API-KEY': API_KEY ?? ''
+        },
+        body: JSON.stringify({
+            userId, title, completed: false
+        })
+    })
+
+    const newTodo: Todo = await res.json()
+
+    return NextResponse.json(newTodo)
+}
+
+export async function PUT(request: Request){
+    const {id,userId, title, completed}: Todo = await request.json()
+
+    if (!id || !title || typeof completed !== 'boolean' || !userId) {  
+        return NextResponse.json({"message": "Missing required data"})
+    }
+
+  const res =  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json',
+            'API-KEY': API_KEY ?? ''
+        },
+        body: JSON.stringify({
+      title, completed, userId
+        })
+    })
+
+    const updatedTodo: Todo = await res.json()
+
+    return NextResponse.json(updatedTodo)
+}
